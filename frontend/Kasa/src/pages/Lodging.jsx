0f56760f25286121a65309a953371e9663f16e1f
CédricC <syman77680@gmail.com>
2026-04-17 15:21:14 +0200

    added fetch for lodging page

--- frontend/Kasa/src/pages/Lodging.jsx
+++ frontend/Kasa/src/pages/Lodging.jsx
@@ -1,10 +1,26 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Lodging() {
     const { id } = useParams();
+    const [property, setProperty] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/properties/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                setProperty(data);
+            })
+            .catch(error => {
+                console.error('Error fetching lodging data:', error);
+            });
+    }, [id]);
 
     return (
-        <h1>Logement : {id}</h1>
+        <div>
+            <h1>Logement : {id}</h1>
+            <img src={property.cover} alt={property.title} />
+        </div>
     );
 }
 
